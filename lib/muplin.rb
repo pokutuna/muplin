c4b816356e0ed65cb7708008d3bb8654cb9f618d
pokutuna <popopopopokutuna@gmail.com>
2011-10-19 10:33:08 +0900

    hoge

--- lib/muplin.rb
+++ lib/muplin.rb
@@ -5,7 +5,7 @@ require 'muplin/outliner'
 module Muplin
 
   def self.exec(src_file, outline, dest_file = 'muplined.pdf')
-    doc = Muplin.Outliner.new(src_file)
+    doc = Muplin::Outliner.new(src_file)
     doc.clear_outlines
     raise ArgumentError 'invalid outline' unless doc.instance_eval { sections? outline }
     doc.define_outline(outline)
